074526853dfbd5bff6b9a6d3a79faec72429134b
JosephMainaOtundo <otundomainajoseph@gmail.com>
2024-05-26 09:53:15 +0300

    Tests passing

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    return [num for num in num_list if num % 2 == 0]
 
 def make_exclamation(sentence_list):
-    pass
+    if sentence_list is None:
+        return []
+    return [sentence + '!' for sentence in sentence_list]
+
+
